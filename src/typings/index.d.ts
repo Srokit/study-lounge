0f10db062245d3d0affcd538822d89dc057746e8
srokit <srok35@gmail.com>
2016-08-13 12:34:42 -0400

    added typings

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -2,5 +2,6 @@
 /// <reference path="globals/express/index.d.ts" />
 /// <reference path="globals/form-data/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/q/index.d.ts" />
 /// <reference path="globals/request/index.d.ts" />
 /// <reference path="globals/serve-static/index.d.ts" />
